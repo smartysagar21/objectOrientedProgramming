8fc2e664c62246dad277122cef0c4ba4a1f3297a
Shwet Sagar <105374331+smartysagar21@users.noreply.github.com>
2023-06-23 22:51:59 +0530

    Class01_03.cpp

---
+++ Class01_03.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+class A {
+	int a;
+public:
+	A() {
+		a = 5;
+	}
+	friend void modify();
+};
+
+void modify() {
+	A obj1;
+	cout << "Private member of Class A, " << obj1.a;
+}
+
+int main() {
+	A obj1;
+	//cout<<obj1.a<<endl;
+
+	modify();
+
+	return 0;
+}
