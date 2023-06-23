c1312f317077067f7f2507291d1cb19e49308302
Shwet Sagar <105374331+smartysagar21@users.noreply.github.com>
2023-06-23 22:51:06 +0530

    Class01_02.cpp

---
+++ Class01_02.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class A {
+
+public:
+	int a, b;
+	A(int a, int b) {
+		this -> a = a;
+		this -> b = b;
+	}
+
+	int getSum() {
+		return (a + b);
+	}
+
+};
+
+int main() {
+	A obj1(3, 7);
+
+	A *ptr;
+	ptr = &obj1;
+
+	(*ptr).a = 5;  // *ptr.a will bring error    //(*ptr).a == ptr -> a
+
+	cout << obj1.getSum() << endl;
+
+	return 0;
+}
