8baa4107d87a48d531d1cc619f8ef721446874f0
Shwet Sagar <105374331+smartysagar21@users.noreply.github.com>
2023-06-23 22:54:51 +0530

    Class01_07.cpp

---
+++ Class01_07.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class A {
+public:
+	static int a;
+	void increment() {
+		a++;
+	}
+	int get() {
+		return a;
+	}
+};
+
+int A::a = 10;  //static member just be called with class name
+
+int main() {
+	A obj1, obj2, obj3;
+
+	obj1.increment();
+	obj2.increment();
+	obj3.increment();
+
+	cout << obj3.get() << " " << obj2.get() << " " << obj1.get() << endl;
+
+	return 0;
+}
